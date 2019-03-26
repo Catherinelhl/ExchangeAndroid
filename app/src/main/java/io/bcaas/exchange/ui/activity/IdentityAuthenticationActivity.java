4e5d414e42ffc1493bf631ba62830ed1790f251d
catherineliu <catherineliu@orangeblock.tech>
2019-03-26 18:49:16 +0800

    身份认证

--- app/src/main/java/io/bcaas/exchange/ui/activity/IdentityAuthenticationActivity.java
+++ app/src/main/java/io/bcaas/exchange/ui/activity/IdentityAuthenticationActivity.java
@@ -1,8 +1,23 @@
 package io.bcaas.exchange.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.jakewharton.rxbinding2.view.RxView;
 import io.bcaas.exchange.R;
 import io.bcaas.exchange.base.BaseActivity;
+import io.bcaas.exchange.constants.Constants;
+import io.bcaas.exchange.listener.OnItemSelectListener;
+import io.bcaas.exchange.view.dialog.DoubleButtonDialog;
+import io.bcaas.exchange.view.viewGroup.SafetyCenterItemView;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+import java.util.concurrent.TimeUnit;
 
 /*
 +--------------+---------------------------------
@@ -14,13 +29,24 @@ import io.bcaas.exchange.base.BaseActivity;
 +--------------+---------------------------------
 + packageName  +   io.bcaas.exchange.ui.activity
 +--------------+---------------------------------
-+ description  +  
++ description  +  身份认证
 +--------------+---------------------------------
 + version      +  
 +--------------+---------------------------------
 */
 
 public class IdentityAuthenticationActivity extends BaseActivity {
+    @BindView(R.id.ib_back)
+    ImageButton ibBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.ib_right)
+    ImageButton ibRight;
+    @BindView(R.id.rl_header)
+    RelativeLayout rlHeader;
+    @BindView(R.id.sciv_authentication)
+    SafetyCenterItemView scivAuthentication;
+
     @Override
     public int getContentView() {
         return R.layout.activity_identity_authentication;
@@ -33,6 +59,10 @@ public class IdentityAuthenticationActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        ibBack.setVisibility(View.VISIBLE);
+        tvTitle.setVisibility(View.VISIBLE);
+        tvTitle.setText(getString(R.string.identity_verification));
+        scivAuthentication.setTabStatusByText(false, getString(R.string.Immediate_authentication));
 
     }
 
@@ -43,6 +73,32 @@ public class IdentityAuthenticationActivity extends BaseActivity {
 
     @Override
     public void initListener() {
+        Disposable subscribe = RxView.clicks(ibBack).throttleFirst(Constants.Time.sleep800, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        setResult(true);
+                    }
+                });
+        scivAuthentication.setOnItemSelectListener(new OnItemSelectListener() {
+            @Override
+            public <T> void onItemSelect(T type, String from) {
+                showDoubleButtonDialog(getString(R.string.Immediate_authentication), new DoubleButtonDialog.ConfirmClickListener() {
+                    @Override
+                    public void sure() {
+                        showToast(getString(R.string.authentication_success));
+                        finish();
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        });
 
     }
+
+
 }
